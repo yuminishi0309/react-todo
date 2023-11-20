1fe3083b0595d2be29e9b50b43515ce314641c81
YumiN <yuminishi0309@gmail.com>
2023-11-20 21:44:45 +0900

    add: todo-function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, toggleTodo }) => {
   return (
-    todos.map((todo) => <Todo todo={todo} key={todo} />)
+    todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo}/>)
   )
 };
 
